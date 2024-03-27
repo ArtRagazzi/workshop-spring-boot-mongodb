271cae88f5622117c59ea1823895796c0d20aeb8
Artur Oliveira <ao.ragazzi@gmail.com>
2024-03-27 15:40:17 -0300

    Obtendo usuario por Id

--- src/main/java/com/artragazzi/workshopmongo/resources/UserResource.java
+++ src/main/java/com/artragazzi/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import com.artragazzi.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,12 @@ public class UserResource {
         return ResponseEntity.ok().body(listDto);
         
     }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
     
 }
--- src/main/java/com/artragazzi/workshopmongo/services/UserService.java
+++ src/main/java/com/artragazzi/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.artragazzi.workshopmongo.services;
 
 import com.artragazzi.workshopmongo.domain.User;
 import com.artragazzi.workshopmongo.repository.UserRepository;
+import com.artragazzi.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,4 +20,12 @@ public class UserService {
 
 
     }
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return user.get();
+    }
+
 }
