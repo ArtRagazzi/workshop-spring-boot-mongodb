ba661dcb02227a3cb94eba89f10d00133f30d6f8
Artur Oliveira <ao.ragazzi@gmail.com>
2024-03-28 02:45:50 -0300

    Adicionando comentarios aos posts

--- src/main/java/com/artragazzi/workshopmongo/domain/Post.java
+++ src/main/java/com/artragazzi/workshopmongo/domain/Post.java
@@ -1,10 +1,13 @@
 package com.artragazzi.workshopmongo.domain;
 
 import com.artragazzi.workshopmongo.dto.AuthorDTO;
+import com.artragazzi.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -15,6 +18,8 @@ public class Post {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
+
 
     public Post(){}
 
@@ -77,5 +82,9 @@ public class Post {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 }
